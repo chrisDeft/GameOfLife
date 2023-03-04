2b93c0fc99c567986203833322cea1830517a797
Chris Adamson <chris.adamson@matrixtelematics.com>
2023-03-04 16:38:24 +0000

    Initial Commit

---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Matrix on 04/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
